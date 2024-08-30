ef7d150e201907f5666cd2f4aa8f51c96926b586
maddiepb <mpallotta-barton@unc.edu>
2024-08-30 15:16:23 -0400

    Finished ex01

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -3,6 +3,16 @@
 __author__ = 730487315
 
 
+def main_planner(guests: int) -> None:
+    """returns number of tea bags, treats, and cost of a party
+    for user input number of people"""
+
+    print("A Cozy Tea Party for " + str(guests) + " People!")
+    print("Tea Bags: " + str(tea_bags(guests)))
+    print("Treats: " + str(treats(guests)))
+    print("Cost: $" + str(cost(tea_count=tea_bags(guests), treat_count=treats(guests))))
+
+
 def tea_bags(people: int) -> int:
     """returns number of tea bags given number of people"""
     return 2 * people
@@ -19,4 +29,11 @@ def treats(people: int) -> int:
 
 def cost(tea_count: int, treat_count: int) -> float:
     """calculates cost of treats and tea bags"""
-    return tea_bags(people=6) * 0.50 + treats(people=6) * 0.75
+    return tea_count * 0.50 + treat_count * 0.75
+
+
+# must call tea_bags and treats functions as arguments for the two parameters
+# when calling the cost function, NOT when defining it
+
+if __name__ == "__main__":
+    main_planner(guests=int(input("How many guests are attending your tea party? ")))
