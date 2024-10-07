367eae2d0c20fb06630a542965c4514a50a4bf2e
maddiepb <mpallotta-barton@unc.edu>
2024-10-07 10:29:23 -0400

    finished cq06

---
+++ CQs/cq06_sum.py
@@ -0,0 +1,32 @@
+"""Summing the elements of a list using different loops"""
+
+__author__ = "730487315"
+
+li: list[float] = [5.5, 5.0, 0.5]
+empt: list[float] = []
+
+
+def w_sum(vals: list[float]):
+    """Uses a while loop to return sum of a given list of floats"""
+    idx: int = 0
+    sum: float = 0.0
+    while idx < len(vals):
+        sum += vals[idx]
+        idx += 1
+
+    return sum
+
+
+def f_sum(vals: list[float]):
+    """Uses a for...in... loop to return sum of a given list of floats"""
+    sum: float = 0.0
+    for elem in vals:
+        sum += elem
+    return sum
+
+
+def f_range_sum(vals: list[float]):
+    sum: float = 0.0
+    for idx in range(0, len(vals)):
+        sum += vals[idx]
+    return sum
